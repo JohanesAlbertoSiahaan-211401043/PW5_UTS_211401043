9bf0392f1c7f4f9e34ba8b822410ce0d4978c1ed
JohanesAlbertoSiahaan-211401043 <114633707+JohanesAlbertoSiahaan-211401043@users.noreply.github.com>
2022-11-09 10:41:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// to select button in accordion
+const accordionHead = document.querySelectorAll(".accordion button");
+// to select img in accordion
+const accordionImg = document.querySelectorAll(".accordion img");
+
+// to add .active in img inside accordion
+accordionImg[0].classList.add('active')
+
+// loop
+for (let i = 0; i < accordionHead.length; i++) {
+  // function that we want to start
+  accordionHead[i].addEventListener("click", function () {
+
+    for(let j = 0; j < accordionImg.length; j++){
+      // to remove active class from img inside accordion
+      accordionImg[j].classList.remove('active')
+    }
+
+    // to add active class from img inside accordion
+    accordionImg[i].classList.add("active");
+  })
+}
